c26df359852b4eb729aa7b74064a6a916168d279
LitQoo <litqoo@gmail.com>
2018-11-22 15:37:41 +0900

    정답 버튼들 추가

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
     public Text text; 
+    public UnityEngine.UI.Text buttonText1;
+    public UnityEngine.UI.Text buttonText2;
+    public UnityEngine.UI.Text buttonText3;
 
 	// Use this for initialization
 	void Start () {
         int a = Random.Range(1, 10);
         int b = Random.Range(1, 10);
+        int answer = a + b;
+
+        buttonText1.text = answer.ToString();
+        buttonText2.text = (answer + 1).ToString();
+        buttonText3.text = (answer - 1).ToString();
 
         text.text = a + " + " + b;
     }
