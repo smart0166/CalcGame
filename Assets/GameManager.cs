37c2deb65dbb0766b56287c494ee83dcfa90d7b8
qorlghd123 <qorlghd123@gmail.com>
2018-11-22 15:28:30 +0900

    숫자 2개 띄우기

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
+
+    public Text text; 
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        int a = Random.Range(1, 10);
+        int b = Random.Range(1, 10);
+
+        text.text = a + " + " + b;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
