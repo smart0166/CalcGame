5a4958ac67bce8b29e5bec3963049827b54fc5e9
LitQoo <litqoo@gmail.com>
2018-11-22 16:04:38 +0900

    정답 위치 랜덤으로 표시

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -15,9 +15,16 @@ public class GameManager : MonoBehaviour {
         int b = Random.Range(1, 10);
         int answer = a + b;
 
-        buttonText1.text = answer.ToString();
-        buttonText2.text = (answer + 1).ToString();
-        buttonText3.text = (answer - 1).ToString();
+        List<int> answers = new List<int>();
+        answers.Add(answer);
+        answers.Add(answer + 1);
+        answers.Add(answer - 1);
+
+        Shuffle(answers);
+
+        buttonText1.text = answers[0].ToString();
+        buttonText2.text = answers[1].ToString();
+        buttonText3.text = answers[2].ToString();
 
         text.text = a + " + " + b;
     }
@@ -26,4 +33,18 @@ public class GameManager : MonoBehaviour {
     void Update () {
 		
 	}
+
+    private System.Random rng = new System.Random();  
+
+    public void Shuffle(IList<int> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            int value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+    }
 }
